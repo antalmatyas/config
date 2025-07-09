8844071997578c720ebf35a37a4413bcec2a22a7
Matyas Antal <latna.matyas@gmail.com>
2025-07-09 10:00:30 +0000

    add foot, hypr, nvim, waybar conf

---
+++ nvim/init.lua
@@ -0,0 +1,3 @@
+require("nconf")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
---
+++ nvim/lua/nconf/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nconf.lazy_init")
+require("nconf.set")
+require("nconf.remap")
